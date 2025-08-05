ab049f29251ffe8cebbc7e7c04812ba47f69f977
Rafael J Torres <rafaelt88@gmail.com>
2025-08-04 21:11:08 -0400

    Fix manifest

--- bss_hospital/__manifest__.py
+++ bss_hospital/__manifest__.py
@@ -13,12 +13,12 @@
         "security/ir.model.access.csv",
         "data/sequence.xml",
         "views/patient_views.xml",
-        "views/patient_readonly_views.xml",https://certificacioninternacional.mijp.gob.ve/paginas/CU_login/
+        "views/patient_readonly_views.xml",
         "views/appointment_views.xml",
         "views/appointment_line_views.xml",
         "views/patient_tag_views.xml",
         "views/account_move_views.xml",
-        "views/menu.xml",https://certificacioninternacional.mijp.gob.ve/paginas/CU_login/
+        "views/menu.xml",
     ],
     'images': ['static/description/banner.jpg'],
     "installable": True,
